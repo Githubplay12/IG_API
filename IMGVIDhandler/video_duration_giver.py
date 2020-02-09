91c22eac0f4e36e0a777464768a06a01311976e6
Githubplay12 <31169929+Githubplay12@users.noreply.github.com>
2020-02-08 20:16:25 -0500

    Update video_duration_giver.py

--- IMGVIDhandler/video_duration_giver.py
+++ IMGVIDhandler/video_duration_giver.py
@@ -1,8 +1,5 @@
 import ffprobe3
 
-videolink = r'C:\Users\CARBON\Desktop\30218427_2117692945128183_4838014671619358720_n.mp4'
-
-
 def return_duration(vid):
     meta = ffprobe3.FFProbe(vid)
     duration = None
